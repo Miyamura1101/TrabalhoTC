b98d3f9d734e3fc2ba613d04b87e3d6c5c96a869
VictorTechrb <victortechrb@gmail.com>
2024-10-04 17:35:24 -0300

    Foi adicionada uma abertura para o explorador de arquivos, no qual ele terá um filtro para selecionar apenas arquivos 'xml'.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,32 @@
 package main.java;
+
+import javax.swing.JFileChooser;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        
+
         minimizacao minimizacao = new minimizacao();
-        Automato afd = new LerArquivoAFD().LerAFD();
-        
-        minimizacao.criarLista(afd);
-    }       
+
+        // Abre o explorador de arquivos para selecionar um arquivo XML
+        File arquivoSelecionado = abrirExploradorDeArquivos();
+
+    }
+
+    // Função para abrir o explorador de arquivos
+    public static File abrirExploradorDeArquivos() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Selecione um arquivo JFLAP (.xml)");
+
+        // Filtrar apenas arquivos com extensão .xml
+        fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Arquivos XML", "xml"));
+
+        int resultado = fileChooser.showOpenDialog(null);
+
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile();
+        } else {
+            return null;
+        }
+    }
 }
