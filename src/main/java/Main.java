d96c4a298858c3fbae3fca6a017b815fcebe7c34
Miyamura1101 <78867396+Miyamura1101@users.noreply.github.com>
2024-10-29 15:22:47 -0300

    Java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
 
-        SeletorDeArquivo seletorDeArquivo = new SeletorDeArquivo();
+       // SeletorDeArquivo seletorDeArquivo = new SeletorDeArquivo();
     
         ArquivoAFD afd = new ArquivoAFD();
         Automato afds = afd.LerAFD();
